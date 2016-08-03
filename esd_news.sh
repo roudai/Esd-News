760644a57f3d7954c94abe3a0bc43fd7108f7595
roudai <me@roudai.net>
2016-08-03 16:20:53 +0900

    Modified grep of icon

--- esd_news.sh
+++ esd_news.sh
@@ -4,11 +4,11 @@ URL="http://comic-earthstar.jp/esd/"
 href=$(curl -s ${URL}news.php | grep -m1 -B2 'class="new"' | sed -e '2,3d' | cut -f2 -d"\"")
 if [ ! -e latest ] || [ ${href} != $(cat latest) ]; then
   content=$(curl -s ${URL}news.php | grep -m1 -A3 ${href} | sed -e '1,3d' -e 's/<[^>]*>//g' -e 's/^[ ]*//g')
-  icon=$(curl -s ${URL}news.php | grep -m1 -A2 ${href} | sed -e '1,1d' -e '3,3d' \
+  icon=$(curl -s ${URL}news.php | grep -m1 -A1 ${href} | sed -e '1,1d' \
                                 | cut -f4 -d"\"" | cut -f3 -d"/" | sed -e "s/_icon.png//")
   case "${icon}" in
     "anime" ) type="【アニメ出演情報】" ;;
-    "cd"    ) type="【CD情報】"  ;;
+    "cd"    ) type="【CD情報】" ;;
     "event" ) type="【イベント出演情報】" ;;
     "game"  ) type="【ゲーム出演情報】" ;;
     "goods" ) type="【グッズ情報】" ;;
